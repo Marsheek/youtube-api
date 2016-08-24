d877e094aad7cc126fc49923b1d406916c925d4c
Marshall Gray <marshallgray1992@gmail.com>
2016-08-24 11:53:00 +1000

    created Video model, view and controller, configured Video, updated views

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get 'videos/index'
+
+  get 'videos/new'
+
+  get 'videos/create'
+
+  resources :videos, only: [:index, :new, :create]
+  root to: 'videos#index'
+
 end
